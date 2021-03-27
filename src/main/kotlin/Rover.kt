c248bce8da066dc816275abe660926cf40f56fc3
Jolanda Wilfer <matejo@Air-von-Jolanda.fritz.box>
2021-03-27 22:17:51 +0100

    initializing rover

---
+++ src/main/kotlin/Rover.kt
@@ -0,0 +1,1 @@
+data class Rover(val x: Int, val y: Int, val facing: String)
--- src/test/groovy/RoverTest.groovy
+++ src/test/groovy/RoverTest.groovy
@@ -7,4 +7,12 @@ class RoverTest extends Specification{
         true
     }
 
+    def "can initialize rover with x=1 y = 1 and facing north" () {
+        def rover = new Rover(1,1, "north")
+        expect:
+        rover.getX() == 1
+        rover.getY() == 1
+        rover.getFacing() == "north"
+    }
+
 }
