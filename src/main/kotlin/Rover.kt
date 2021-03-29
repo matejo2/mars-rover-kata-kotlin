7f8ac9134d4fb0c450b47936bc82b9e2539a8724
matejo <kixhot4Blau>
2021-03-29 21:53:58 +0200

    refactoring

--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -2,13 +2,8 @@ data class Rover(val x: Int, val y: Int, val facing: FACING) {
 
     fun receiveInput(input: String, rover: Rover): Rover {
         if (input.contentEquals("r")) {
-            if (rover.facing == FACING.WEST) {
-                return Rover(rover.x, rover.y, FACING.NORTH)
-            }
-            var newFacing = FACING.values()[rover.facing.ordinal + 1]
-            return Rover(rover.x, rover.y, newFacing)
+            return turnRight(rover)
         }
-
         if (input.contentEquals("b")) {
             return Rover((rover.x - 1), rover.y, rover.facing)
         }
@@ -16,6 +11,13 @@ data class Rover(val x: Int, val y: Int, val facing: FACING) {
             return Rover((rover.x + 1), rover.y, rover.facing)
         }
         return rover
+    }
+
+    private fun turnRight(rover: Rover): Rover {
+        if (rover.facing == FACING.WEST) {
+            return Rover(rover.x, rover.y, FACING.values()[0])
+        }
+        return Rover(rover.x, rover.y, FACING.values()[rover.facing.ordinal+1])
     }
 }
 
