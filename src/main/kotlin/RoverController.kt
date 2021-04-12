124d69e1852efe88cd93389e053b510d514ba9ce
matejo <kixhot4Blau>
2021-04-12 16:53:37 +0200

    deleted unessearcy files

--- src/main/kotlin/RoverController.kt
+++
@@ -1,7 +0,0 @@
-class RoverController {
-    fun move(input: String, rover: Rover): Rover {
-        //var returnRover = Rover(1,1,FACING.NORTH)
-        //input.forEach { returnRover = rover.receiveInput(it) }
-        return rover
-    }
-}
